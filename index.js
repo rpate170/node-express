f8b1f24cbad65b8c4c7ef5ec2b6e05e7fc0f958f
rpate170 <rushi.patel175@gmail.com>
2020-10-01 16:24:22 -0400

    Assignment 1

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const morgan = require('morgan');
 const bodyParser = require('body-parser');
 
 const dishRouter = require('./routes/dishRouter');
+const promoRouter = require('./routes/promoRouter');
+const leaderRouter = require('./routes/leaderRouter');
 
 const hostname = 'localhost';
 const port = 3000;
@@ -14,6 +16,8 @@ app.use(bodyParser.json());
 
 //mounting the router
 app.use('/dishes', dishRouter);
+app.use('/promotions', promoRouter);
+app.use('/leaders', leaderRouter);
 
 app.use(express.static(__dirname+ '/public'));
 
